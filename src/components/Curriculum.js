b4e9d84542891b4278a09367c7d0df172d6edd65
alexander <aldml0201@gmail.com>
2023-09-07 14:07:17 -0400

    ¨correcciones¨

--- src/components/Curriculum.js
+++ src/components/Curriculum.js
@@ -2,49 +2,54 @@ import React from 'react'
 
 export const Curriculum = () => {
   return (
-    <div className='page'>
-      <h1 className='heading'>Curriculum</h1>
-      <ul>
-        <li>
-
-        </li>
-      </ul>
-      <h2>Experiencias</h2>
-      <ul>
-        <li>
-          
-        </li>
-      </ul>
-      <h2>Conocimientos</h2>
-      <ul>
-        <li>
-          
-        </li>
-      </ul>
-      <h2>Educación</h2>
-      <ul>
-        <li>
-          
-        </li>
-      </ul>
-      <h2>Proyectos personales</h2>
-      <ul>
-        <li>
-          
-        </li>
-      </ul>
-      <h2>Reconocimientos y premios</h2>
-      <ul>
-        <li>
-          
-        </li>
-      </ul>
-      <h2>Otros</h2>
-      <ul>
-        <li>
-          
-        </li>
-      </ul>
-    </div>
+    <>
+      <div className='page'>
+        <h1 className='heading'>Curriculum</h1>
+        <ul>
+          <li>
+          1 año de experiencia siendo Teacher Asistent en un bootcamp de programacion. buena capacidad adaptativa para entornos de trabajo en equipo. curiosidad por seguir aprendiendo las tecnologia que ya manejo y explorar nuevas.
+          </li>
+        </ul>
+        <h2>Experiencias</h2>
+        <ul>
+          <p>
+          apoyar a los alumnos en el proceso de aprendizaje, dar charlas motivacionales para que no decaigan en el proceso de aprendizaje, ayudarlos a resolver los diferentes conflictos que puedan surgir en el codigo a lo largo de los proyectos, correccion de proyectos
+          </p>
+        </ul>
+        <h2>Conocimientos</h2>
+        <ul className='Lknowledge'>
+          <li className="js"><i class="fa-brands fa-js"></i></li>
+          <li className="nodejs"><i class="fa-brands fa-node-js"></i></li>
+          <li className="react"><i class="fa-brands fa-react"></i></li>
+          <li className="html"><i class="fa-brands fa-html5"></i></li>
+          <li className="css"><i class="fa-brands fa-css3-alt"></i></li>
+          <li className="bootstrap"><i class="fa-brands fa-bootstrap"></i></li>
+        </ul>
+        <h2>Educación</h2>
+        <ul>
+          <li>
+          Full Stack Sofware Development, 4Geeks Academy
+          </li>
+        </ul>
+        <h2>Proyectos personales</h2>
+        <ul>
+          <li>
+          Plantaforma de Psicologos enfocada en brindarle herramientas para realizar mejor su trabajo
+          </li>
+          <li>
+          Tecnologias utilizadas: HTML,CSS,REACT, JavaScript, Python, SqlAchemy, Flask
+          </li>
+        </ul>
+        <h2>Lenguajes</h2>
+        <ul>
+          <li>
+          Español, Nativo
+          </li>
+          <li>
+          Ingles, Intermedio
+          </li>
+        </ul>
+      </div>
+    </>
   )
 }
