99684ee64b5fab256dbe5ee6fdb250698c0c45ad
alexander <aldml0201@gmail.com>
2023-09-07 11:59:30 -0400

    estructura

---
+++ src/router/MisRutas.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import {Routes, Route, BrowserRouter, Navigate } from "react-router-dom"
+import { Inicio } from '../components/Inicio'
+import { Portafolio } from '../components/Portafolio'
+import { Servicio } from '../components/Servicio'
+import { Curriculum } from '../components/Curriculum'
+import { Contacto } from '../components/Contacto'
+import { HeaderNav } from '../components/layout/HeaderNav'
+import { Footer } from '../components/layout/Footer'
+import { Proyecto } from '../components/Proyecto'
+
+export const MisRutas = () => {
+  return (
+    <BrowserRouter>
+        {/* header y Navegacion */}
+            <HeaderNav/>
+        {/* contenido central */}
+        <section className='content'>
+            <Routes>
+                <Route path="/" element={<Navigate to="/Inicio"/>} />
+                <Route path="/inicio" element={<Inicio/>} />
+                <Route path="/portafolio" element={<Portafolio/>} />
+                <Route path="/servicios" element={<Servicio/>} />
+                <Route path="/Curriculum" element={<Curriculum/>} />
+                <Route path="/contacto" element={<Contacto/>} />
+                <Route path="/proyecto/:id" element={<Proyecto/>} />
+                <Route path="*" element={
+                  <div className='page'>
+                    <h1 className='heading'>error 404 </h1>
+                  </div>
+                } />
+
+            </Routes>
+        </section>
+        
+        {/* pie de pagina */}
+        <Footer/>
+    </BrowserRouter>
+  )
+}
